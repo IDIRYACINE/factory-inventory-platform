af7c75e4bc09fbe1c8c4b64d18514a340679d4dd
IDIRYACINE <idiryacinesp@gmail.com>
2023-09-25 21:45:07 +0000

    init

---
+++ frontend/next.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const { StatsWriterPlugin } = require("webpack-stats-plugin");
+const Visualizer = require('webpack-visualizer-plugin2');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+
+
+    // const analyzeBundles = process.env.ANALYZE_BUNDLE
+
+    // if (analyzeBundles) {
+    //   const timestamp = Date.now()
+
+    //   config.plugins.push(
+    //     new StatsWriterPlugin({
+    //       filename: path.join('..', 'stats', `${timestamp}.json`),
+    //       fields: null,
+    //       stats: { chunkModules: true },
+    //     })
+    //   )
+
+    //   config.plugins.push(
+    //     new Visualizer({
+    //       filename: path.join('..', 'stats', `${timestamp}.html`),
+    //     })
+    //   )
+
+
+
+    // }
+
+    // if (process.env.WEBPACK_BUNDLE) {
+    //   config.plugins.push(new BundleAnalyzerPlugin())
+    // }
+
+    return config
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default async function Home() {
+
+  return (
+
+    <main className="flex flex-col items-center justify-center">
+     
+    </main>
+  )
+}
+
